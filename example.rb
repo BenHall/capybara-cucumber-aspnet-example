bfaa601e8d4781acf6f1e7590080339bf9008a0e
BenHall <Ben@Benhall.me.uk>
2010-05-31 18:57:45 +0100

    Bing example

--- example.rb
+++ example.rb
@@ -1,7 +1,8 @@
 require 'rubygems'
 require 'capybara'
 require 'capybara/dsl'
-
+require "selenium-webdriver"
+ 
 include Capybara
 
 Capybara.debug = true
@@ -9,17 +10,18 @@ Capybara.log 'test'
 
 #Capybara.default_driver = :culerity
 Capybara.default_driver = :selenium
+#Selenium::WebDriver.for :chrome <-- didn't work
 Capybara.use_default_driver
-Capybara.app_host = 'http://www.google.com'
+Capybara.app_host = 'http://www.bing.com'
 puts Capybara.default_driver
 
 
 visit '/'
 #fill_in 'q', :with => 'Test'
 #click_button 'Google Search'
-locate("//input[@name='q']").set('Test hack')
+locate("//input[@name='q']").set('Test')
 # This also works:
   # b = locate('//input[@title="Google Search"]')
   # b.node.send_keys('ESC')
-click_button 'Google Search'
+click_button 'sb_form_go'
 puts page.source.length
